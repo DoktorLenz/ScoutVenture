957848773b9a64dbbc960033852d424480336c7f
DoktorLenz <laurenz.stinner2@gmail.com>
2025-02-02 19:54:34 +0100

    Input and register forms good for mobile

--- frontend/ScoutVenture/src/app/auth/login/login.component.ts
+++ frontend/ScoutVenture/src/app/auth/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { AutoFocusModule } from 'primeng/autofocus';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { FluidModule } from 'primeng/fluid';
 import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'sv-login',
@@ -15,8 +18,20 @@ import { InputTextModule } from 'primeng/inputtext';
     FloatLabelModule,
     FluidModule,
     RouterLink,
+    PasswordModule,
+    AutoFocusModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  protected loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.warn(this.loginForm.value);
+  }
+}
--- frontend/ScoutVenture/src/app/auth/register/register.component.ts
+++ frontend/ScoutVenture/src/app/auth/register/register.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { AutoFocusModule } from 'primeng/autofocus';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { FluidModule } from 'primeng/fluid';
 import { InputTextModule } from 'primeng/inputtext';
+import { Password, PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'sv-register',
@@ -15,8 +18,30 @@ import { InputTextModule } from 'primeng/inputtext';
     FloatLabelModule,
     FluidModule,
     RouterLink,
+    PasswordModule,
+    AutoFocusModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  protected registerForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    repeatPassword: new FormControl(''),
+  });
+
+  @ViewChild('passwordInput') protected passwordInput!: Password;
+  @ViewChild('repeatPasswordInput') protected repeatPasswordInput!: Password;
+
+  // protected focusPassword() {
+  //   this.passwordInput.el.nativeElement.querySelector('input').focus();
+  // }
+
+  // protected focusRepeatPassword() {
+  //   this.repeatPasswordInput.el.nativeElement.querySelector('input').focus();
+  // }
+
+  protected onSubmit() {}
+}
