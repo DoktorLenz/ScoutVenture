51bcedd99042427142a05674889dc38e191e769e
DoktorLenz <laurenz.stinner2@gmail.com>
2025-03-16 20:45:41 +0100

    guard for auth

--- frontend/ScoutVenture/src/app/shared/guards/auth.guard.ts
+++ frontend/ScoutVenture/src/app/shared/guards/auth.guard.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
+import { catchError, map } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
-  return router.navigate(['/auth/login'], {
-    queryParams: { redirect: state.url },
-  });
+  const http = inject(HttpClient);
+
+  return http.get<void>('/api/manage/info').pipe(
+    map(() => true),
+    catchError(() =>
+      router.navigate(['/auth/login'], {
+        queryParams: { redirect: state.url },
+      })
+    )
+  );
 };
