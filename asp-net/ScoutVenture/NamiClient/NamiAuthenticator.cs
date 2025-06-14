100377989af274bcb687639ff0e5fc879c0bdb02
DoktorLenz <laurenz.stinner2@gmail.com>
2025-06-14 16:24:01 +0200

    Nami Login

---
+++ asp-net/ScoutVenture/NamiClient/NamiAuthenticator.cs
@@ -0,0 +1,75 @@
+﻿using System.Net;
+using System.Security.Authentication;
+using System.Text.Json;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace NamiClient
+{
+    public class NamiAuthenticator(string memberId, string password, CookieContainer cookieContainer) : IAuthenticator
+    {
+        private bool _authenticated;
+        private readonly object _lock = new();
+
+        public ValueTask Authenticate(IRestClient client, RestRequest request)
+        {
+            lock (_lock)
+            {
+                if (_authenticated) return ValueTask.CompletedTask;
+                
+                // use a separate client to send authentication request 
+                var loginClientOptions = new RestClientOptions(client.Options.BaseUrl!)
+                {
+                    CookieContainer = cookieContainer
+                };
+                
+                using var loginClient = new RestClient(loginClientOptions);
+                
+                var loginRequest = new RestRequest(NamiApiEndpoints.Login, Method.Post);
+                loginRequest.AddHeader("Content-Type", ContentType.FormUrlEncoded);
+                
+                loginRequest.AddParameter("username", memberId);
+                loginRequest.AddParameter("password", password);
+                loginRequest.AddParameter("Login", "API");
+                
+                var loginResponse = loginClient.ExecuteAsync(loginRequest).Result;
+
+                var activateLoginRequest = new RestRequest(loginResponse.GetHeaderValue("Location"));
+                var activateLoginResponse = loginClient.ExecuteAsync(activateLoginRequest).Result;
+                
+                VerifyLogin(activateLoginResponse);
+
+                _authenticated = true;
+            }
+
+            return ValueTask.CompletedTask;
+        }
+
+        private void VerifyLogin(RestResponse response)
+        {
+            var statusCode = response.StatusCode;
+
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.NoContent:
+                    throw new Exception("Session activation error");
+                case HttpStatusCode.OK:
+                    break;
+                default:
+                    throw new Exception("Unexpected response status code: " + response.StatusCode);
+            }
+
+            if (cookieContainer.GetAllCookies().All(c => c.Name != "JSESSIONID"))
+            {
+                throw new Exception("No session cookie received");
+            }
+
+            NamiLoginResponse? namiLoginResponse = JsonSerializer.Deserialize<NamiLoginResponse>(response.Content!);
+            
+            if (namiLoginResponse == null)
+            {
+                throw new Exception("Empty body");
+            }
+        }
+    }
+}
---
+++ asp-net/ScoutVenture/NamiClient/NamiRestClient.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using RestSharp;
+
+namespace NamiClient;
+
+public class NamiRestClient : IDisposable
+{
+    readonly RestClient _restClient;
+    
+    public NamiRestClient(string memberId, string password)
+    {
+        var cookieContainer = new CookieContainer();
+        var options = new RestClientOptions("https://nami.dpsg.de")
+        {
+            CookieContainer = cookieContainer,
+            FollowRedirects = false,
+            Authenticator = new NamiAuthenticator(memberId, password, cookieContainer)
+        };
+        _restClient = new RestClient(options);
+    }
+
+    public Task<RestResponse> GetAllNamiMembersOfGrouping(string groupingId)
+    {
+        var request = new RestRequest(NamiApiEndpoints.AllMembersOfGrouping(groupingId));
+        return _restClient.ExecuteAsync(request);
+    }
+
+    public void Dispose()
+    {
+        _restClient.Dispose();
+    }
+}
